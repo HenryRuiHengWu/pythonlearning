eb11a9c2b312c766479041462a775e3f1cf813cd
HenryRuiHengWu <21910069@zju.edu.cn>
2020-11-02 21:24:31 +0800

    version1

---
+++ .vscode/test.py
@@ -0,0 +1,32 @@
+#!/Users/admin/AppData/Local/Programs/Python/Python39
+# -*- encoding: utf-8 -*-
+'''
+@File    :   test.py
+@Time    :   2020/11/02 14:21:26
+@Author  :   HenryWu 
+@Version :   1.0
+@Contact :   21910069@zju.edu.cn
+'''
+# Start typing your code from here
+
+
+print("Hello world!")
+for i in range(10):
+    print(i)
+print("wtf")
+
+print("version2")
+
+print("version3")
+
+print("version4")
+
+print("version5")
+
+def x():
+    pass
+
+
+def y():
+    pass
+
