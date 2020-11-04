aa79ef7a8f50b4421a2114d9f2c3738d297b1089
HenryRuiHengWu <21910069@zju.edu.cn>
2020-11-04 11:11:02 +0800

    20201104

---
+++ .vscode/basic.py
@@ -0,0 +1,15 @@
+#!/Users/admin/AppData/Local/Programs/Python/Python39
+# -*- encoding: utf-8 -*-
+'''
+@File    :   basic.py
+@Time    :   2020/11/03 21:16:17
+@Author  :   HenryWu 
+@Version :   1.0
+@Contact :   21910069@zju.edu.cn
+'''
+# Start typing your code from here
+
+
+
+print('''name
+...henry''')
